1c94f44df53f3da4522c6e812de256f1eec10846
GustavoW <guvieirawalter@gmail.com>
2025-03-25 18:43:53 -0300

    Código refatorado

---
+++ Diamante/GerarDiamante.cs
@@ -0,0 +1,40 @@
+﻿namespace Diamante.ConsoleApp
+{
+    public static class GerarDiamante
+    {
+        public static int ColetarValor(int valor = 0)
+        {
+            Console.Clear();
+            Console.Write("Informe um número ímpar: ");
+            if (!int.TryParse(Console.ReadLine(), out int tamanho))
+            {
+                Console.WriteLine("Número INTEIRO inválido. \nAperte ENTER para tentar novamente");
+                Console.ReadLine();
+            }
+
+            if (tamanho % 2 == 0)
+            {
+                Console.WriteLine("Número informado não é ímpar. \nAperte ENTER para continuar");
+                Console.ReadLine();
+            }
+            valor = tamanho;
+            return valor;
+        }
+        public static void MostrarDiamante(int tamanho)
+        {
+            int meio = tamanho / 2;
+
+            for (int i = 0; i <= meio; i++)
+            {
+                string linha = $"{new string(' ', meio - i)}{new string('x', 2 * i + 1)}";
+                Console.WriteLine(linha);
+            }
+
+            for (int i = meio - 1; i >= 0; i--)
+            {
+                string linha = $"{new string(' ', meio - i)}{new string('x', 2 * i + 1)}";
+                Console.WriteLine(linha);
+            }
+        }
+    }
+}
--- Diamante/Program.cs
+++ Diamante/Program.cs
@@ -7,35 +7,11 @@
 
             while (true)
             {
-
-                Console.Clear();
-                Console.Write("Informe um número ímpar: ");
-                int tamanho = Convert.ToInt32(Console.ReadLine());
-
-                if (tamanho % 2 == 0)
-                {
-                    Console.WriteLine("Número informado não é ímpar. \nAperte ENTER para continuar");
-                    Console.ReadLine();
-                    continue;
-                }
-
-                int meio = tamanho / 2;
-
-                for (int i = 0; i <= meio; i++)
-                {
-                    string linha = $"{new string(' ', meio - i)}{new string('x', 2 * i + 1)}";
-                    Console.WriteLine(linha);
-                }
-
-                for (int i = meio - 1; i >= 0; i--)
-                {
-                    string linha = $"{new string(' ', meio - i)}{new string('x', 2 * i + 1)}";
-                    Console.WriteLine(linha);
-                }
+                int tamanho = GerarDiamante.ColetarValor();
+                GerarDiamante.MostrarDiamante(tamanho);
 
                 Console.Write("\nDeseja continuar (S/N): ");
                 string continuar = Console.ReadLine().ToUpper();
-
                 if (continuar != "S")
                 {
                     break;
