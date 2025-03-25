7e3d6424042a633721d11dc0d4b8780364149c1a
GustavoW <guvieirawalter@gmail.com>
2025-03-25 17:34:08 -0300

    Resolvida lógica de gerar o diamante e verificar se o número é ímpar

--- Diamante/Program.cs
+++ Diamante/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            while (true)
+            {
+
+                Console.Clear();
+                Console.Write("Informe um número ímpar: ");
+                int tamanho = Convert.ToInt32(Console.ReadLine());
+
+                if (tamanho % 2 == 0)
+                {
+                    Console.WriteLine("Número informado não é ímpar. \nAperte ENTER para continuar");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                int meio = tamanho / 2;
+
+                for (int i = 0; i <= meio; i++)
+                {
+                    string linha = $"{new string(' ', meio - i)}{new string('x', 2 * i + 1)}";
+                    Console.WriteLine(linha);
+                }
+
+                for (int i = meio - 1; i >= 0; i--)
+                {
+                    string linha = $"{new string(' ', meio - i)}{new string('x', 2 * i + 1)}";
+                    Console.WriteLine(linha);
+                }
+
+                Console.Write("\nDeseja continuar (S/N): ");
+                string continuar = Console.ReadLine().ToUpper();
+
+                if (continuar != "S")
+                {
+                    break;
+                }
+
+            }
         }
     }
 }
